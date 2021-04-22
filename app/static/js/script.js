5c55614bd57f81a18882cd083b5c02f9b3d608ca
magicwenli <yxra3603@outlook.com>
2021-04-22 21:22:35 +0800

    local video stream is available

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -17,4 +17,32 @@ $(document).ready(function () {
         $(".popOut").css("display", "none");
         // $(".popOutBg").style.display = "none";
     });
+
+    var $btn_video = $('#btn-video');
+    var local_stream;
+    var $local_video = $('#local-video')[0];
+    var remote_stream;
+    var $remote_video = $('#remote-video')[0];
+
+    $btn_video.on("click", function () {
+        $(".video-screen").removeClass("d-none");
+        $(".video-screen").addClass("d-block");
+        getLocalVideo();
+    })
+
+    function getLocalVideo() {
+        var constraints = {audio: true, video: {width: 247, height: 156}};
+
+        navigator.mediaDevices.getUserMedia(constraints)
+            .then(function (mediaStream) {
+                $local_video.srcObject = mediaStream;
+                $local_video.onloadedmetadata = function (e) {
+                    $local_video.play();
+                };
+            })
+            .catch(function (err) {
+                console.log(err.name + ": " + err.message);
+            }); // always check for errors at the end.
+    }
+
 })
