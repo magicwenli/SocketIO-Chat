f484bf8de3613febdf6c50d12c727adf86618c4c
magicwenli <yxra3603@outlook.com>
2021-04-24 20:06:32 +0800

    add video stop switch

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -19,15 +19,25 @@ $(document).ready(function () {
     });
 
     var $btn_video = $('#btn-video');
+    var video_on = 0;
     var local_stream;
     var $local_video = $('#local-video')[0];
     var remote_stream;
     var $remote_video = $('#remote-video')[0];
 
     $btn_video.on("click", function () {
-        $(".video-screen").removeClass("d-none");
-        $(".video-screen").addClass("d-block");
-        getLocalVideo();
+        if (video_on === 0) {
+            $(".video-screen").removeClass("d-none");
+            $(".video-screen").addClass("d-block");
+            getLocalVideo();
+            video_on = 1;
+        } else {
+            $(".video-screen").removeClass("d-block");
+            $(".video-screen").addClass("d-none");
+            stopLocalVideo();
+            video_on = 0;
+        }
+
     })
 
     function getLocalVideo() {
@@ -35,7 +45,8 @@ $(document).ready(function () {
 
         navigator.mediaDevices.getUserMedia(constraints)
             .then(function (mediaStream) {
-                $local_video.srcObject = mediaStream;
+                local_stream = mediaStream;
+                $local_video.srcObject = local_stream;
                 $local_video.onloadedmetadata = function (e) {
                     $local_video.play();
                 };
@@ -43,6 +54,14 @@ $(document).ready(function () {
             .catch(function (err) {
                 console.log(err.name + ": " + err.message);
             }); // always check for errors at the end.
+    }
+
+    function stopLocalVideo() {
+        if (local_stream.active) {
+            local_stream.getTracks()[0].stop(); // stop video
+            local_stream.getTracks()[1].stop(); // stop audio
+        }
+
     }
 
 })
